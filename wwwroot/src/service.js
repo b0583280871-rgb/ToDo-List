1f3da8a869291887c3b619644aef863b98564540
Your Name <you@example.com>
2026-04-23 18:47:33 +0300

    Initial commit - Todo List Project

---
+++ wwwroot/src/service.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+//axios.defaults.baseURL = "http://localhost:5296";
+
+const apiUrl = "http://localhost:5296"
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`${apiUrl}/items`)
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    // console.log('addTask', name)
+    //TODO
+    const result = await axios.post(`${apiUrl}/items/`, { name: name })
+
+    return result.data;
+  },
+
+  setCompleted: async (id, isComplete, name) => {
+    // console.log('setCompleted', {id, isComplete})
+    //TODO
+    const result = await axios.put(`${apiUrl}/items/${id}`,
+      {
+        IdItems: id,
+        Name: name,
+        IsComplete: isComplete
+      })
+    return result.data;
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask', id);
+    // שליחת בקשת DELETE לכתובת עם ה-ID הספציפי
+    await axios.delete(`${apiUrl}/items/${id}`);
+    return {}; // מחזירים אובייקט ריק כדי שהקוד ב-App.js לא יישבר
+  }
+};
